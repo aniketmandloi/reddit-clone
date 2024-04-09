841e122beefd74866fba55449589ec29d59aa361
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-04-09 15:46:44 +0530

    added microORM instance

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "redditclone",
+    // type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
